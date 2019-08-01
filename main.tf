142696baf0ae3cd47a6bd27a119a71a98650edbf
Circular-test <nmartinez@corpit-consulting.com>
2019-08-01 16:55:47 -0300

    Fix cache cluster size

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@ resource "aws_api_gateway_stage" "test" {
   rest_api_id   = "${var.rest_api_id}"
   deployment_id = "${var.deployment_id}"
   variables     = "${var.variables}"
+  cache_cluster_size = "${var.cache_cluster_size}"
 }
