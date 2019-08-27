c935b31b2add260dd5f88b83bcf2a9007579c443
Circular-test <nmartinez@corpit-consulting.com>
2019-08-27 12:19:23 -0300

    fix support cluster_size and support xray_tracing_enabled

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 resource "aws_api_gateway_stage" "test" {
-  stage_name    = "${var.stage_name}"
-  rest_api_id   = "${var.rest_api_id}"
-  deployment_id = "${var.deployment_id}"
-  variables     = "${var.variables}"
-  cache_cluster_size = "${var.cache_cluster_size}"
+  stage_name            = "${var.stage_name}"
+  rest_api_id           = "${var.rest_api_id}"
+  deployment_id         = "${var.deployment_id}"
+  variables             = "${var.variables}"
+  cache_cluster_size    = "${var.cache_cluster_size}"
+  cache_cluster_enabled = "${var.cache_cluster_enabled}"
+  xray_tracing_enabled  = "${var.xray_tracing_enabled}"
 }
 
 
